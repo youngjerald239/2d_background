97414fd5db0b6380be2f125535eb23b5d8801735
youngjerald239 <youngjerald239@gmail.com>
2022-08-30 20:32:27 -0500

    added scroll correction

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 800
 const CANVAS_HEIGHT = canvas.height = 700
-let gameSpeed = 5
+let gameSpeed = 15
 
 const backgroundLayer1 = new Image()
 backgroundLayer1.src = './assets/layer-1.png'
@@ -22,9 +22,9 @@ function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     ctx.drawImage(backgroundLayer4, x, 0)
     ctx.drawImage(backgroundLayer4, x2, 0)
-    if(x < - 2400) x = 2400
+    if(x < - 2400) x = 2400 + x2 - gameSpeed
     x -= gameSpeed
-    if(x2 < - 2400) x2 = 2400
+    if(x2 < - 2400) x2 = 2400 + x - gameSpeed
     x2 -= gameSpeed
     requestAnimationFrame(animate)
 }
