5f0db1ce55ca11fd71e2210dc4f6c743f1719ad5
youngjerald239 <youngjerald239@gmail.com>
2022-08-30 21:24:41 -0500

    added layer background and loop

--- script.js
+++ script.js
@@ -43,12 +43,18 @@ class Layer {
     }
 }
 
+const layer1 = new Layer(backgroundLayer1, 0.5)
+const layer2 = new Layer(backgroundLayer2, 0.5)
+const layer3 = new Layer(backgroundLayer3, 0.5)
 const layer4 = new Layer(backgroundLayer4, 0.5)
+const layer5 = new Layer(backgroundLayer5, 1)
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
     layer4.update()
-    layer4.draw()    
+    layer4.draw() 
+    layer5.update()
+    layer5.draw()    
     requestAnimationFrame(animate)
 }
 animate()
