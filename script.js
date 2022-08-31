4fa9247927ce5ab67697e5e3c0781c1543497d51
youngjerald239 <youngjerald239@gmail.com>
2022-08-30 20:29:01 -0500

    add scroll on background

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 800
 const CANVAS_HEIGHT = canvas.height = 700
+let gameSpeed = 5
+
+const backgroundLayer1 = new Image()
+backgroundLayer1.src = './assets/layer-1.png'
+const backgroundLayer2 = new Image()
+backgroundLayer2.src = './assets/layer-2.png'
+const backgroundLayer3 = new Image()
+backgroundLayer3.src = './assets/layer-3.png'
+const backgroundLayer4 = new Image()
+backgroundLayer4.src = './assets/layer-4.png'
+const backgroundLayer5 = new Image()
+backgroundLayer5.src = './assets/layer-5.png'
+
+let x = 0
+let x2 = 2400
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+    ctx.drawImage(backgroundLayer4, x, 0)
+    ctx.drawImage(backgroundLayer4, x2, 0)
+    if(x < - 2400) x = 2400
+    x -= gameSpeed
+    if(x2 < - 2400) x2 = 2400
+    x2 -= gameSpeed
+    requestAnimationFrame(animate)
+}
+animate()
