1f7441515f1fa81bcd5319c944ee9232ebad52aa
youngjerald239 <youngjerald239@gmail.com>
2022-08-30 21:26:49 -0500

    added multiple layers

--- script.js
+++ script.js
@@ -49,12 +49,15 @@ const layer3 = new Layer(backgroundLayer3, 0.5)
 const layer4 = new Layer(backgroundLayer4, 0.5)
 const layer5 = new Layer(backgroundLayer5, 1)
 
+const gameObjects = [layer1, layer2, layer3, layer4, layer5]
+
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    layer4.update()
-    layer4.draw() 
-    layer5.update()
-    layer5.draw()    
+    gameObjects.forEach(object => {
+        object.update()
+        object.draw()
+    }) 
+
     requestAnimationFrame(animate)
 }
 animate()
