53be559b93d48d98018dfab162016359f8f1a7f2
youngjerald239 <youngjerald239@gmail.com>
2022-08-30 21:15:37 -0500

    added constructor variable

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1')
 const ctx = canvas.getContext('2d')
 const CANVAS_WIDTH = canvas.width = 800
 const CANVAS_HEIGHT = canvas.height = 700
-let gameSpeed = 15
+let gameSpeed = 10
 
 const backgroundLayer1 = new Image()
 backgroundLayer1.src = './assets/layer-1.png'
@@ -15,17 +15,36 @@ backgroundLayer4.src = './assets/layer-4.png'
 const backgroundLayer5 = new Image()
 backgroundLayer5.src = './assets/layer-5.png'
 
-let x = 0
-let x2 = 2400
+class Layer {
+    constructor(image, speedModifier) {
+        this.x = 0
+        this.y = 0
+        this.width = 2400
+        this.height = 700
+        this.x2 = this.width
+        this.image = image
+        this.speedModifier = speedModifier
+        this.speed = gameSpeed * this.speedModifier
+    }
+    update(){
+        this.speed = gamespeed * this.speedModifier
+        if (this.x <= -this.width){
+            this.x = this.width + this.x2 - this.speed
+        }
+        if (this.x2 <= -this.width){
+            this.x2 = this.width + this.x - this.speed
+        }
+        this.x = Math.floor(this.x - this.speed)
+        this.x2 = Math.floor(this.x2 - this.speed)
+    }
+    draw(){
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+        ctx.drawImage(this.image, this.x2, this.y, this.width, this.height)
+    }
+}
 
 function animate(){
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    ctx.drawImage(backgroundLayer4, x, 0)
-    ctx.drawImage(backgroundLayer4, x2, 0)
-    if(x < - 2400) x = 2400 + x2 - gameSpeed
-    x -= gameSpeed
-    if(x2 < - 2400) x2 = 2400 + x - gameSpeed
-    x2 -= gameSpeed
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)    
     requestAnimationFrame(animate)
 }
 animate()
